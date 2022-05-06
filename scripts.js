c9505f646b6f7ebf41da44da9554f9323475ef13
devid99eneas <devid99eneas@gmail.com>
2022-05-06 15:23:47 -0300

    comentarios adicionados, ainda falta adicionar os nomes no star

--- scripts.js
+++ scripts.js
@@ -1,49 +1,68 @@
-const cellElements = document.querySelectorAll("[data-cell]"); /*Seleciona todos as divs que tem essa classe, logo todos os cell */
+const cellElements = document.querySelectorAll("[data-cell]"); 
+//Seleciona todos as divs que tem essa classe, logo todos os cell
 const board = document.querySelector("[data-board]");
-const winningMessageTextElement = document.querySelector(
-  "[data-winning-message-text]"
-);
-const winningMessage = document.querySelector("[data-winning-message]");
-const restartButton = document.querySelector("[data-restart-button]");
+/*Seleciona todos as divs que tem essa classe, logo todos os boards
 
-let isCircleTurn;
+Ligação com a mensagem devitporia ou empete*/
+const winningMessageTextElement = document.querySelector("[data-winning-message-text]");
+//Seleciona todos as divs que tem essa classe, logo todos os winning-message-text
+const winningMessage = document.querySelector("[data-winning-message]");
+//Seleciona todos as divs que tem essa classe, logo todos os winning-message
+const restartButton = document.querySelector("[data-restart-button]");
+/*Seleciona todos as divs que tem essa classe, logo todos os restart-button
+
+Ligação com os inputs de nome*/
+const inputNameTextElement = document.querySelector("[data-inputName-message-text]");
+//Seleciona todos as divs que tem essa classe, logo todos os inputName-message-text
+const inputNameMessage = document.querySelector("[data-inputName-message]");
+//Seleciona todos as divs que tem essa classe, logo todos os inputName-message
+const cadastroButton = document.querySelector("[data-cadastro-button]");
+//Seleciona todos as divs que tem essa classe, logo todos os cadastro-button
+
+let vezDoCirculo;//É a vez do circulo ?
 
 const winningCombinations = [
-  /*   A  B  C      */
-  /*1.[0, 1, 2]
+  /*Combinações que leva um jogador a ganhar,
+   caso nenhuma delas esteja no resultado sera contabilizado empate
+       A  B  C      
+    1.[0, 1, 2]
     2.[3, 4, 5]
     3.[6, 7, 8]*/
 
-  [0, 1, 2],/*linha 1*/
-  [3, 4, 5],/*linha 2*/
-  [6, 7, 8],/*linha 3*/
-  [0, 3, 6],/*Coluna A*/
-  [1, 4, 7],/*Coluna B*/
-  [2, 5, 8],/*Coluna C*/
-  [0, 4, 8],/*Diagonal principal*/
-  [2, 4, 6],/*Diagonal secundaria*/
+  [0, 1, 2],//linha 1
+  [3, 4, 5],//linha 2
+  [6, 7, 8],//linha 3
+  [0, 3, 6],//Coluna A
+  [1, 4, 7],//Coluna B
+  [2, 5, 8],//Coluna C
+  [0, 4, 8],//Diagonal principal
+  [2, 4, 6],//Diagonal secundaria
 ];
-
+function mensagemStarGame(nomeUm,nomeDois){
+  inputNameMessage.classList.remove("show-inputName-message")
+}
 const startGame = () => {
-  isCircleTurn = false;
-/*Para (Instaceia uma variavel) da estrutura que eu quero percorrer [OBS.: for of retorna o valor da possição || for in retorna apenas as posições]*/
+ // mensagemStarGame(nomeUm,nomeDois);
+
+  vezDoCirculo = false;
+//Para (Instaceia uma variavel) da estrutura que eu quero percorrer [OBS.: for of retorna o valor da possição || for in retorna apenas as posições]
   for (const cell of cellElements) {
-    /**/
     cell.classList.remove("circle");
     cell.classList.remove("x");
     cell.removeEventListener("click", handleClick);
     cell.addEventListener("click", handleClick, { once: true });
+                                                 //once - responsavel por não adiconar uma classe quando já tiver
   }
 
   setBoardHoverClass();
   winningMessage.classList.remove("show-winning-message");
 };
-
-const endGame = (isDraw) => {
+//Mensagem no final do jogo
+const encerraPartida = (isDraw) => {
   if (isDraw) {
     winningMessageTextElement.innerText = "Empate!";
   } else {
-    winningMessageTextElement.innerText = isCircleTurn
+    winningMessageTextElement.innerText = vezDoCirculo
       ? "O Venceu!"
       : "X Venceu!";
   }
@@ -65,47 +84,47 @@ const checkForDraw = () => {
   });
 };
 
-const placeMark = (cell, classToAdd) => {
+function placeMark (cell, classToAdd) {
   cell.classList.add(classToAdd);
 };
-
-const setBoardHoverClass = () => {
+//Pesquisar qual a diferença de estrutura
+function setBoardHoverClass ()  {
+  //Remove a classe anterior antes de adicionar uma nova
   board.classList.remove("circle");
   board.classList.remove("x");
-
-  if (isCircleTurn) {
+  //Faz uma verificação é adiciona uma classe
+  if (vezDoCirculo) {
     board.classList.add("circle");
   } else {
     board.classList.add("x");
   }
 };
 
-const swapTurns = () => {
-  isCircleTurn = !isCircleTurn;
-
+const mudarJogador = () => {
+  vezDoCirculo = !vezDoCirculo;
   setBoardHoverClass();
 };
 
 const handleClick = (e) => {
   // Colocar a marca (X ou Círculo)
   const cell = e.target;
-  const classToAdd = isCircleTurn ? "circle" : "x";
+  const classToAdd = vezDoCirculo ? "circle" : "x"; //If e Else; verifica se é a vez do circula é adiciona a calsse corespondente
 
-  placeMark(cell, classToAdd);
+  placeMark(cell, classToAdd); //Chama a função
 
   // Verificar por vitória
-  const isWin = checkForWin(classToAdd);
+  const vencedor = checkForWin(classToAdd);
 
   // Verificar por empate
   const isDraw = checkForDraw();
 
-  if (isWin) {
-    endGame(false);
+  if (vencedor) {
+    encerraPartida(false);
   } else if (isDraw) {
-    endGame(true);
+    encerraPartida(true);
   } else {
-    // Mudar símbolo
-    swapTurns();
+    // Mudar jogador
+    mudarJogador();
   }
 };
 
